4e09ca98f687a1810fe2cf478c0b88546426d21a
Nioce <awilom6175@greenvilleschools.us>
2018-03-06 16:16:34 -0500

    Add files via upload

---
+++ FRQ's/BatterLife_2009/BatteryCharger.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class BatteryCharger here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BatteryCharger
+{
+    private int[] rateTable;
+    private int getChargingCost(int startHour, int chargeTime){
+        int cost = 0;
+        for(int i = 0; chargeTime > i; i++){
+            
+        }
+    }
+    
+    public int getChargeStartTime(int chargeTime){
+        int startTime= 0;
+        
+    }
+}
